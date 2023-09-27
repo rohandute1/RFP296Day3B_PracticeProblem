c349914d25834df1dd64d26d4eb7ef335a577b7a
Rohan <rohandute1@gmail.com>
2023-09-27 15:01:05 +0530

    [rohan]initial commit

---
+++ Day_3B_Practice_Problem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3B_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 3B Practice Problems");
+            Console.ReadLine();
+        }
+    }
+}
