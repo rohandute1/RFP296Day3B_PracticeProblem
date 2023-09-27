022d8d2dd2a8a34d85b82dfe7fe12868e5c49f40
Rohan <rohandute1@gmail.com>
2023-09-27 15:21:53 +0530

    [rohan]successfully done demonstration of constructor types

---
+++ Day_3B_Practice_Problem/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3B_Practice_Problem
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Default Constructor
+        public Person()
+        {
+            Name = "Ganesh";
+            Age = 30;
+        }
+
+        // Parameterized Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Copy Constructor
+        public Person(Person otherPerson)
+        {
+            Name = otherPerson.Name;
+            Age = otherPerson.Age;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
--- Day_3B_Practice_Problem/Program.cs
+++ Day_3B_Practice_Problem/Program.cs
@@ -10,7 +10,48 @@ namespace Day_3B_Practice_Problem
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Day 3B Practice Problems");
+            bool continueExecution = true;
+            while (continueExecution)
+            {
+                Console.WriteLine("Please choose any one of the following program:");
+                Console.WriteLine("1.Types of Constructor\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        // Using the default constructor
+                        Person person1 = new Person();
+                        Console.WriteLine("Default Constructor:");
+                        person1.Display();
+
+                        // Using the parameterized constructor
+                        Person person2 = new Person("Ram", 25);
+                        Console.WriteLine("\nParameterized Constructor:");
+                        person2.Display();
+
+                        // Using the copy constructor
+                        Person person3 = new Person(person2);
+                        Console.WriteLine("\nCopy Constructor:");
+                        person3.Display();
+                        break;
+
+                    
+
+                    default:
+                        Console.WriteLine("Invalid option, Please select a valid program.");
+                        break;
+                }
+
+                Console.WriteLine("Do you want to continue?(yes or no)");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
+
             Console.ReadLine();
         }
     }
